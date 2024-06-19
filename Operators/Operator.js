f7f3eb2770563cfd8944c79a674e166389389e9e
NisargaHRShetty <159692971+NisargaHRShetty@users.noreply.github.com>
2024-06-19 19:48:06 +0530

    Add files via upload

---
+++ Operators/Operator.js
@@ -0,0 +1,21 @@
+function operators(a, b) {
+
+    console.log('Addition:',a+b);
+    console.log('Subtraction:',a-b);
+    console.log('Multiplication:',a*b);
+    console.log('Division:',a/b);
+    console.log('Modulus:',a%b);
+    console.log('Exponential:',a**b);
+    
+    //Logical operators
+    console.log('Logical AND (a && b:)', a && b);
+    console.log('Logical OR (a || b:)', a || b);
+    console.log('Logical NOT (!a :)', !a);
+
+
+}
+    let a = 10;
+    let b = 3;
+    operators(a,b); 
+
+
